b77da44a8f3e2b4a271b758fc10cdf1ea66f0c35
Rob Hoelz <rob@hoelz.ro>
2011-09-19 23:35:50 -0500

    Add forgotten example files

---
+++ examples/module-template-after.pm
@@ -0,0 +1,18 @@
+package Foo::Bar;
+
+use strict;
+use warnings;
+
+1;
+
+__END__
+
+# ABSTRACT:
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 METHODS
+
+=cut
---
+++ examples/module-template-before.pm
@@ -0,0 +1,36 @@
+package Foo::Bar;
+
+use strict;
+use warnings;
+
+our $VERSION = '0.01';
+
+1;
+
+__END__
+
+=head1 NAME
+
+Foo::Bar -
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 METHODS
+
+=head1 BUG REPORTING
+
+...
+
+=head1 COPYING
+
+...
+
+=head1 AUTHOR
+
+Rob Hoelz <rob@hoelz.ro>
+
+=head1 SEE ALSO
+
+L<Foo>
